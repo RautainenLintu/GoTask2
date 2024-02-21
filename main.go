bc2fb876e6db67541e9880aa09adf3205d4f9052
Ekaterina Batulina <katya_batulina@mail.ru>
2024-02-21 22:56:40 +0300

    Main logic

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
+func askForInput(name string) float64 {
+	var number float64
+	fmt.Printf("Enter your %s: ", name)
+	fmt.Scan(&number)
+	return number
+}
+
 func main() {
-	var income int
-	fmt.Print("Enter your income: ")
-	fmt.Scan(&income)
+	income := askForInput("income")
+	food := askForInput("food cost")
+	transport := askForInput("transportation cost")
+	accommodation := askForInput("accommodation cost")
+	totalExpenses := food + transport + accommodation
+	diff := income - totalExpenses
+	fmt.Printf("Your surplus is: %f \n", diff)
 
-	// var food int
-	// var transport int
-	// var accommodation int
-
+	var expenses = [3]float64{food, transport, accommodation}
+	var categories = [3]string{"food", "transport", "accommodation"}
+	for index, item := range expenses {
+		share := item / income
+		if share >= 0.2 {
+			fmt.Printf("Expenses are too high in the category: %s. They consist %f of your income.\n", categories[index], share)
+		}
+	}
 }
