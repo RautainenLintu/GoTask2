d2c8a22d98aeb9454b977cfe02c4c802c2f9a1dc
Ekaterina Batulina <katya_batulina@mail.ru>
2024-02-21 22:35:52 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var income int
+	fmt.Print("Enter your income: ")
+	fmt.Scan(&income)
+
+	// var food int
+	// var transport int
+	// var accommodation int
+
+}
